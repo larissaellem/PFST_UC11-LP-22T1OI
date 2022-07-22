97c1695243dc2bbc7f56034a88ca08ecfbfea089
Larissa Ellen <larissaellen415@gmail.com>
2022-07-22 16:24:37 -0300

    resolvendo ativividade encontro remoto 1

---
+++ encontro-remoto-1/pecas.js
@@ -0,0 +1,21 @@
+let pesoPeca = 100
+let numeroPeca = 17
+let nomePeca = "Pa";
+
+if (pesoPeca > 100) {
+    console.log("Pode-se cadastrar a peça")
+} else {
+    console.log("Peso insuficiente. ") 
+}
+
+if (numeroPeca >= 10) {
+    console.log("Numero de peças excedido")
+} else {
+    console.log("Cadastro permitido")
+}
+
+if (nomePeca.length < 3) {
+    console.log("Numero de caracteres insuficiente")
+} else {
+    console.log("Cadastro conluído")
+}
